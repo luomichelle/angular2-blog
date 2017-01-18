a01c7eda333a9d3af600ad8039a30a34e34bad0f
Eric Zhang <beefjava@gmail.com>
2017-01-18 22:45:29 +0800

    add article detials switch animation

--- src/app/article/article-detail/article-detail.component.ts
+++ src/app/article/article-detail/article-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding,
+         trigger, transition, animate,
+         style, state } from '@angular/core';
 
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
@@ -9,10 +11,44 @@ import { Article } from '../article';
   templateUrl: './article-detail.component.html',
   styleUrls: [
     './article-detail.component.scss'
+  ],
+  animations: [
+    trigger('routeAnimation', [
+      state('*',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })
+      ),
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100%)'
+        }),
+        animate('0.2s ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.5s ease-out', style({
+          opacity: 0,
+          transform: 'translateY(100%)'
+        }))
+      ])
+    ])
   ]
 })
 
-export class ArticleDetailComponent {
+export class ArticleDetailComponent implements OnInit {
+  @HostBinding('@routeAnimation') get routeAnimation() {
+    return true;
+  }
+
+  @HostBinding('style.display') get display() {
+    return 'block';
+  }
+
+  @HostBinding('style.position') get position() {
+    return 'absolute';
+  }
 
   article: Article;
 
