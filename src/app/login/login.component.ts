c68ed695ee4401b9d9f1a828d37870e809a57798
Eric Zhang <beefjava@gmail.com>
2017-01-21 00:06:59 +0800

    modify login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { User } from './user';
 import { fadeIn } from '../animations/fade-in';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'login',
@@ -17,7 +18,8 @@ export class LoginComponent {
   localState: any;
   submitted: Boolean = false;
   loginUser: User;
-  constructor(public route: ActivatedRoute) {
+  message: string;
+  constructor(public route: ActivatedRoute, public authService: AuthService, public router: Router) {
      this.loginUser = new User(18, '', '', '');
   }
 
@@ -30,25 +32,10 @@ export class LoginComponent {
       });
 
     console.log('hello `login` component');
-    // static data that is bundled
-    // var mockData = require('assets/mock-data/mock-data.json');
-    // console.log('mockData', mockData);
-    // if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
-    // this.asyncDataWithWebpack();
   }
-  asyncDataWithWebpack() {
-    // you can also async load mock data with 'es6-promise-loader'
-    // you would do this if you don't want the mock-data bundled
-    // remember that 'es6-promise-loader' is a promise
-    setTimeout(() => {
 
-      System.import('../../assets/mock-data/mock-data.json')
-        .then(json => {
-          console.log('async mockData', json);
-          this.localState = json;
-        });
-
-    });
+  sendMessage() {
+    this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
   }
 
   onSubmit() {
@@ -57,6 +44,21 @@ export class LoginComponent {
 
   login() {
     this.loginUser = new User(42, '', '', '');
+    this.message = 'Trying to log in ...';
+
+    this.authService.login()
+                  .subscribe(() => {
+                    this.sendMessage();
+                    if (this.authService.isLoggedIn) {
+                      let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/admin';
+                      this.router.navigate([redirect]);
+                    }
+                  })
+  }
+
+  logout() {
+    this.authService.logout();
+    this.sendMessage();
   }
 
   get diagnostic() {
